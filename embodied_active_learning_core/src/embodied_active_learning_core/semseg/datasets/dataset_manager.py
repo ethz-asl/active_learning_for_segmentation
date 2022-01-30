79762ec3b084be28fcd26408114dcac20cc8e07f
René Zurbrügg <zrene@student.ethz.ch>
2022-01-30 22:38:32 +0100

    Add transform to masks (allows to map semantics e.g. nyu - mp)

--- embodied_active_learning_core/src/embodied_active_learning_core/semseg/datasets/dataset_manager.py
+++ embodied_active_learning_core/src/embodied_active_learning_core/semseg/datasets/dataset_manager.py
@@ -11,16 +11,22 @@ class GenericFromFolderDataset(torch.utils.data.Dataset):
   """ Datloader to load images produced by the data acquisitors"""
 
   def __init__(self, folder_path, num_imgs=None, transform=None, image_ending="image.png", mask_ending="mask.png",
-               weights_ending="weights.npy"):
+               weights_ending="weights.npy", mask_transform_fn = None):
     super().__init__()
     self.entries = []
     for f in sorted(glob.glob(f"{folder_path}/*" + image_ending)):
       mask_file = f.replace(image_ending, mask_ending)
       weights_file = f.replace(image_ending, weights_ending)
+      mask_content =  np.asarray(PImage.open(mask_file)).copy()
+
+      # Allows to transform masks (e.g. nyu <-> eigen13)
+      if mask_transform_fn is not None:
+        mask_content = mask_transform_fn(mask_content)
+
       data = {
         'image': np.asarray(PImage.open(f)).copy(),
-        'mask': np.asarray(PImage.open(mask_file)).copy(),
-        'weight': np.load(weights_file) if os.path.exists(weights_file) else None
+        'mask': mask_content,
+        'weight': np.load(weights_file) if os.path.exists(weights_file) else mask_content
       }
       self.entries.append(data)
     self.transform = transform
@@ -41,5 +47,5 @@ def get_train_dataset_for_folder(dataset_folder, normalize=False):
   return GenericFromFolderDataset(dataset_folder, transform=get_train_transforms(normalize=normalize))
 
 
-def get_test_dataset_for_folder(dataset_folder, normalize=False):
-  return GenericFromFolderDataset(dataset_folder, transform=get_validation_transforms(normalize=normalize))
+def get_test_dataset_for_folder(dataset_folder, normalize=False, mapping_fn = None):
+  return GenericFromFolderDataset(dataset_folder, transform=get_validation_transforms(normalize=normalize), mask_transform_fn = mapping_fn)
